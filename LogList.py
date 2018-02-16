47937daed9f9fd718d506a8f75dfc620efee8c7d
bennpham <bennguyenpham@hotmail.com>
2018-02-16 15:07:21 -0800

    WIP LogList

---
+++ LogList.py
@@ -0,0 +1,15 @@
+import Log
+
+
+class LogList:
+
+    def __init__(self):
+        self.list = []
+
+    def add(self, log):
+        pass
+        # TODO
+
+    def reorder(self):
+        pass
+        # TODO
