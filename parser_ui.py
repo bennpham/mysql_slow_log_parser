deca0a7681b15a1d58f1e4c22fe4f0beba765030
bennpham <bennguyenpham@hotmail.com>
2018-02-16 15:05:50 -0800

    WIP parser_ui with generating default_settings.cfg and displaying user commands (Does nothing yet)

--- parser_ui.py
+++ parser_ui.py
@@ -1,15 +1,38 @@
+import LogList
+import ParserSettings
+
+
 class ParserUI:
     def __init__(self):
-        self.logs = []
+        self.logs = LogList.LogList()
+        self.parser_setting = ParserSettings.ParserSettings()
 
     def run(self):
-        pass
+        # Attempt to load default parser settings
+        try:
+            self.parser_setting.read("cfg/default_setting.cfg")
+        except FileNotFoundError:
+            print("Default settings cannot be found!")
+            print("Generating new default settings...\n")
+            self.generate_new_default_setting()
+
+        # Prompt user for input
+        while True:
+            print("(L)oad new parser settings")
+            print("(S)ave current parser settings")
+            print("(E)dit parser settings")
+            print("(N)ew parser operation")
+            print("(Q)uit")
+            user_input = str(input(">"))
 
     def quit(self):
         pass
 
-    def _load_file(self, filename):
+    def load_file(self, filename):
         pass
 
-    def _load_files(self, folder):
+    def load_files(self, folder):
         pass
+
+    def generate_new_default_setting(self):
+        self.parser_setting.write("cfg/default_setting.cfg")
