0e4aa6c2f9813af6eb610d1fbf041c4afddf1fae
bennpham <bennguyenpham@hotmail.com>
2018-02-19 12:20:48 -0800

    Added options to ask users to change parser settings

--- parser_ui.py
+++ parser_ui.py
@@ -36,13 +36,81 @@ class ParserUI:
                 self.save_current_parser_settings()
             # Edit Parser Settings
             elif user_input.lower().strip() == 'e':
-                pass
+                self.edit_parser_operations()
             # New Parser Operation
             elif user_input.lower().strip() == 'n':
                 pass
             # Quit
             elif user_input.lower().strip() == 'q':
                 break
+
+    def edit_parser_operations(self):
+        print("Order logs by? ('query_time', 'lock_time', 'rows_sent', 'rows_examined', 'datetime')")
+        user_input = str(input(">"))
+        self.parser_setting.order = ParserSettings.sanitize_order(user_input)
+        print("Sort by? ('asc', 'desc')")
+        user_input = str(input(">"))
+        self.parser_setting.sort = ParserSettings.sanitize_sort(user_input)
+        print("Default log name (cannot contain characters \ / : * ? \" < > |")
+        user_input = str(input(">"))
+        self.parser_setting.default_log_name = ParserSettings.sanitize_filename(user_input)
+        print("Output query min time (-1 for no checking)")
+        user_input = str(input(">"))
+        self.parser_setting.output_query_time_min = ParserSettings.sanitize_number(user_input)
+        print("Output query max time (-1 for no checking)")
+        user_input = str(input(">"))
+        self.parser_setting.output_query_time_max = ParserSettings.sanitize_number(user_input)
+        print("Output lock min time (-1 for no checking)")
+        user_input = str(input(">"))
+        self.parser_setting.output_lock_time_min = ParserSettings.sanitize_number(user_input)
+        print("Output lock max time (-1 for no checking)")
+        user_input = str(input(">"))
+        self.parser_setting.output_lock_time_max = ParserSettings.sanitize_number(user_input)
+        print("Output rows sent min time (-1 for no checking)")
+        user_input = str(input(">"))
+        self.parser_setting.output_rows_sent_min = ParserSettings.sanitize_number(user_input)
+        print("Output rows sent time (-1 for no checking)")
+        user_input = str(input(">"))
+        self.parser_setting.output_rows_sent_max = ParserSettings.sanitize_number(user_input)
+        print("Output rows examined min time (-1 for no checking)")
+        user_input = str(input(">"))
+        self.parser_setting.output_rows_examined_min = ParserSettings.sanitize_number(user_input)
+        print("Output rows examined max time (-1 for no checking)")
+        user_input = str(input(">"))
+        self.parser_setting.output_rows_examined_max = ParserSettings.sanitize_number(user_input)
+        print("Output datetime min (YYYY-MM-DD)")
+        user_input = str(input(">"))
+        self.parser_setting.output_datetime_min = ParserSettings.sanitize_datetime(user_input)
+        print("Output datetime max (YYYY-MM-DD)")
+        user_input = str(input(">"))
+        self.parser_setting.output_datetime_max = ParserSettings.sanitize_datetime(user_input)
+        print("Output database user (leave blank for no filters)")
+        user_input = str(input(">"))
+        self.parser_setting.output_database_user = user_input
+        print("Output database host (leave blank for no filters)")
+        user_input = str(input(">"))
+        self.parser_setting.output_database_host = user_input
+        print("Output database name (leave blank for no filters)")
+        user_input = str(input(">"))
+        self.parser_setting.output_database_name = user_input
+        print("Display Datetime ? (0 or 1)")
+        user_input = str(input(">"))
+        self.parser_setting.display_datetime = ParserSettings.sanitize_binary_number(user_input)
+        print("Display Database Host? (0 or 1)")
+        user_input = str(input(">"))
+        self.parser_setting.display_database_host = ParserSettings.sanitize_binary_number(user_input)
+        print("Display Time? (0 or 1)")
+        user_input = str(input(">"))
+        self.parser_setting.display_time = ParserSettings.sanitize_binary_number(user_input)
+        print("Display Database? (0 or 1)")
+        user_input = str(input(">"))
+        self.parser_setting.display_database = ParserSettings.sanitize_binary_number(user_input)
+        print("Display Timestamp? (0 or 1)")
+        user_input = str(input(">"))
+        self.parser_setting.display_timestamp = ParserSettings.sanitize_binary_number(user_input)
+        print("Display Statement? (0 or 1)")
+        user_input = str(input(">"))
+        self.parser_setting.display_statement = ParserSettings.sanitize_binary_number(user_input)
 
     def load_new_parser_settings(self):
         print("Type in the path and filename of the parser settings.")
