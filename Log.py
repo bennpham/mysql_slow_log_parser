12fa976c2183450fb41c1e2223473f65d772c515
bennpham <bennguyenpham@hotmail.com>
2018-02-19 14:02:12 -0800

    Defined Log object

--- Log.py
+++ Log.py
@@ -1,3 +1,15 @@
 class Log:
-    def __init__(self):
-        pass # TODO
+    def __init__(self, datetime="", database_host="", time="", database="", timestamp="", statement="",
+                 query_time=0, lock_time=0, rows_sent=0, rows_examined=0, datetime_value=0):
+        self.datetime = datetime
+        self.database_host = database_host
+        self.time = time
+        self.database = database
+        self.timestamp = timestamp
+        self.statement = statement
+
+        self.query_time = query_time
+        self.lock_time = lock_time
+        self.rows_sent = rows_sent
+        self.rows_examined = rows_examined
+        self.datetime_value = datetime_value
