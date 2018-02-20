5d9fc21f6335c139dcb043c80a37d46a6217b4bf
bennpham <bennguyenpham@hotmail.com>
2018-02-19 18:59:05 -0800

    Created parsing ability & sorting function for Parser

--- Parser.py
+++ Parser.py
@@ -1,3 +1,7 @@
+import Log
+import datetime
+
+
 class Parser:
 
     def __init__(self):
@@ -9,6 +13,53 @@ class Parser:
     def clear(self):
         self.list = []
 
-    def reorder(self):
-        pass
-        # TODO
+    def reorder(self, order, sort):
+        # Define default key
+        key = Log.Log.query_time
+        reverse = True
+
+        if order == "query_time":
+            key = Log.Log.query_time
+        elif order == "lock_time":
+            key = Log.Log.lock_time
+        elif order == "rows_sent":
+            key = Log.Log.rows_sent
+        elif order == "rows_examined":
+            key = Log.Log.rows_examined
+        elif order == "datetime":
+            key = Log.Log.datetime_value
+
+        if sort == 'asc':
+            reverse = False
+        elif sort == 'desc':
+            reverse = True
+
+        self.list.sort(key=key, reverse=reverse)
+
+    def parse(self, file):
+        with open(file, "r") as infile:
+            lines = infile.readlines()[3:]
+            num_of_entries = len(lines) // 6
+            # Fails if remainder in divide by 6 and first line doesn't start with # Time
+            if len(lines) % 6 != 0:
+                return False
+            if len(lines) > 0:
+                if lines[0][0:7] != "# Time:":
+                    return False
+            for i in range(num_of_entries):
+                log = Log.Log()
+                log.datetime = lines[i*6]
+                log.database_host = lines[i*6 + 1]
+                log.time = lines[i*6 + 2]
+                log.database = lines[i*6 + 3]
+                log.timestamp = lines[i*6 + 4]
+                log.statement = lines[i*6 + 5]
+                # Get number values for query_time, lock_time, rows_sent, rows_examined
+                time = lines[i*6 + 2].split()
+                log.query_time = float(time[2])
+                log.lock_time = float(time[4])
+                log.rows_sent = float(time[6])
+                log.rows_examined = float(time[8])
+                log.datetime_value = datetime.datetime(int(log.datetime[8:12]), int(log.datetime[13:15]), int(log.datetime[16:18]),
+                                                       int(log.datetime[19:21]), int(log.datetime[22:24]), int(log.datetime[25:27]))
+                self.add(log)
