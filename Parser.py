d6ae063ad39d2ebfc6d609b11065a7d5b7456fff
bennpham <bennguyenpham@hotmail.com>
2018-02-20 16:50:36 -0800

    Added parser filters & completed mysql parser
    If output_database_user, output_database_host, or output_database_name crashes then I'll probably need a try except there, but it seems like all of the mysql slow query logs output those data.

--- Parser.py
+++ Parser.py
@@ -74,16 +74,67 @@ class Parser:
     def write(self, file, parser_settings):
         with open(file, "w") as outfile:
             for log in self.list:
-                if parser_settings.display_datetime == 1:
-                    outfile.write(log.datetime)
-                if parser_settings.display_database_host == 1:
-                    outfile.write(log.database_host)
-                if parser_settings.display_time == 1:
-                    outfile.write(log.time)
-                if parser_settings.display_database == 1:
-                    outfile.write(log.database)
-                if parser_settings.display_timestamp == 1:
-                    outfile.write(log.timestamp)
-                if parser_settings.display_statement == 1:
-                    outfile.write(log.statement)
-                outfile.write("\n")  # TODO. Write only when filter constraint holds
+                if _check_parser_settings_to_log_values(parser_settings, log):
+                    if parser_settings.display_datetime == 1:
+                        outfile.write(log.datetime)
+                    if parser_settings.display_database_host == 1:
+                        outfile.write(log.database_host)
+                    if parser_settings.display_time == 1:
+                        outfile.write(log.time)
+                    if parser_settings.display_database == 1:
+                        outfile.write(log.database)
+                    if parser_settings.display_timestamp == 1:
+                        outfile.write(log.timestamp)
+                    if parser_settings.display_statement == 1:
+                        outfile.write(log.statement)
+                    outfile.write("\n")
+
+def _check_parser_settings_to_log_values(parser_settings, log):
+    # Query Time Min
+    if log.query_time < parser_settings.output_query_time_min:
+        return False
+    # Query Time Max
+    if log.query_time > parser_settings.output_query_time_max and parser_settings.output_query_time_max != -1:
+        return False
+    # Lock Time Min
+    if log.lock_time < parser_settings.output_lock_time_min:
+        return False
+    # Lock Time Max
+    if log.lock_time > parser_settings.output_lock_time_max and parser_settings.output_lock_time_max != -1:
+        return False
+    # Rows Sent Min
+    if log.rows_sent < parser_settings.output_rows_sent_min:
+        return False
+    # Rows Sent Max
+    if log.rows_sent > parser_settings.output_rows_sent_max and parser_settings.output_rows_sent_max != -1:
+        return False
+    # Rows Examined Min
+    if log.rows_examined < parser_settings.output_rows_examined_min:
+        return False
+    # Rows Examined Max
+    if log.rows_examined > parser_settings.output_rows_examined_max and parser_settings.output_rows_examined_max != -1:
+        return False
+    # Datetime Min
+    if parser_settings.output_datetime_min != '0000-00-00':
+        try:
+            if log.datetime_value > datetime.datetime.strptime(parser_settings.output_datetime_min, '%Y-%m-%d'):
+                return False
+        except:
+            pass
+    # Datetime Max
+    if parser_settings.output_datetime_max != '0000-00-00':
+        try:
+            if log.datetime_value < datetime.datetime.strptime(parser_settings.output_datetime_max, '%Y-%m-%d'):
+                return False
+        except:
+            pass
+    # Output database_user
+    if parser_settings.output_database_user != "" and parser_settings.output_database_user != log.database_host.split()[2]:
+        return False
+    # Output database_host
+    if parser_settings.output_database_host != "" and parser_settings.output_database_host != log.database_host.split()[4]:
+        return False
+    # Output database_name
+    if parser_settings.output_database_name != "" and parser_settings.output_database_name != log.database[4:-2]:
+        return False
+    return True
